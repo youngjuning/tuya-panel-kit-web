1c86b671cdbe4d134ed664666abf4d821f100058
ZengZhao <zengzhao@tuya.com>
2021-04-06 11:32:08 +0800

    wip(Components): Synchronization

--- src/components/layout/react-navigation/TransitionPresets.js
+++ src/components/layout/react-navigation/TransitionPresets.js
@@ -4,10 +4,12 @@ import {
   TransitionSpecs,
   HeaderStyleInterpolators,
 } from '@react-navigation/stack';
+import { RatioUtils } from '../../../utils';
 
 const { add, multiply } = Animated;
+const { isIphoneX, isIos } = RatioUtils;
 
-export const SlideFromRightWithMargin = {
+const SlideFromRightWithMargin = {
   gestureDirection: 'horizontal',
   transitionSpec: {
     open: TransitionSpecs.TransitionIOSSpec,
@@ -40,23 +42,12 @@ export const SlideFromRightWithMargin = {
         : 0
     );
 
-    const translateUnfocused = next
-      ? multiply(
-          next.progress.interpolate({
-            inputRange: [0, 1],
-            outputRange: [0, screen.width * -1],
-            extrapolate: 'clamp',
-          }),
-          inverted
-        )
-      : 0;
-
     const overlayOpacity = progress.interpolate({
       inputRange: [0, 1, 1.0001, 2],
       outputRange: [0, 0.3, 1, 1],
     });
 
-    const statusBarHeight = insets.top;
+    const statusBarHeight = insets.top - (isIos ? 44 : 0);
 
     return {
       cardStyle: {
@@ -66,7 +57,7 @@ export const SlideFromRightWithMargin = {
         borderTopRightRadius: 10,
         marginTop: statusBarHeight + topOffset,
         marginBottom: topOffset,
-        transform: [{ translateX: translateFocused }, { translateX: translateUnfocused }],
+        transform: [{ translateX: translateFocused }],
       },
       overlayStyle: { opacity: overlayOpacity },
     };
@@ -74,7 +65,83 @@ export const SlideFromRightWithMargin = {
   headerStyleInterpolator: HeaderStyleInterpolators.forFade,
 };
 
-export const TransitionPresets = {
+export const ModalPresentationIOS = {
+  gestureDirection: 'vertical',
+  transitionSpec: {
+    open: TransitionSpecs.TransitionIOSSpec,
+    close: TransitionSpecs.TransitionIOSSpec,
+  },
+  cardStyleInterpolator: ({ index, current, next, inverted, layouts: { screen }, insets }) => {
+    const isLandscape = screen.width > screen.height;
+    const topOffset = isLandscape ? 0 : 10;
+    const statusBarHeight = insets.top - (isIos ? 44 : 0);
+    const aspectRatio = screen.height / screen.width;
+
+    const progress = add(
+      current.progress.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0, 1],
+        extrapolate: 'clamp',
+      }),
+      next
+        ? next.progress.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, 1],
+            extrapolate: 'clamp',
+          })
+        : 0
+    );
+
+    const translateY = multiply(
+      progress.interpolate({
+        inputRange: [0, 1, 2],
+        outputRange: [
+          screen.height,
+          index === 0 ? 0 : topOffset,
+          (index === 0 ? statusBarHeight : 0) - topOffset * aspectRatio,
+        ],
+      }),
+      inverted
+    );
+
+    const overlayOpacity = progress.interpolate({
+      inputRange: [0, 1, 1.0001, 2],
+      outputRange: [0, 0.3, 1, 1],
+    });
+
+    const scale = isLandscape
+      ? 1
+      : progress.interpolate({
+          inputRange: [0, 1, 2],
+          outputRange: [1, 1, screen.width ? 1 - (topOffset * 2) / screen.width : 1],
+        });
+
+    const borderRadius = isLandscape
+      ? 0
+      : index === 0
+      ? progress.interpolate({
+          inputRange: [0, 1, 1.0001, 2],
+          outputRange: [0, 0, isIphoneX ? 38 : 0, 10],
+        })
+      : 10;
+
+    return {
+      cardStyle: {
+        overflow: 'hidden',
+        borderTopLeftRadius: borderRadius,
+        borderTopRightRadius: borderRadius,
+        marginTop: index === 0 ? 0 : statusBarHeight,
+        marginBottom: index === 0 ? 0 : topOffset,
+        transform: [{ translateY }, { scale }],
+      },
+      overlayStyle: { opacity: overlayOpacity },
+    };
+  },
+  headerStyleInterpolator: HeaderStyleInterpolators.forFade,
+};
+
+export default {
   ...originTransitionPresets,
   SlideFromRightWithMargin,
+  ModalPresentationIOS,
 };
